ca78c8cc7ad61b1fe9c939e045ec8065191b3226
deepak kumar <deepak9013216128@gmail.com>
2020-03-29 15:07:38 +0000

    add route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-
+import {Route,Switch} from 'react-router-dom';
 import './App.css';
 
 import {generatePalette} from './colorHelper'
-
 import seedColors from './seedColors';
-
 import Palatte from './component/palette/palette.component';
 
 
 function App() {
-	// console.log(generatePalette(seedColors[4]))
+	console.log(generatePalette(seedColors[4]))
   return (
-    <div >
-      <Palatte palette={generatePalette(seedColors[4])} />
-    </div>
+		<Switch>
+			<Route exact path='/' render={()=><h1>home page</h1>}/>
+			<Route exact path='/palette/:id' render={()=><h1>palette page</h1>}/>
+		</Switch>
+		
+    // <div >
+    //   <Palatte palette={generatePalette(seedColors[4])} />
+    // </div>
   );
 }
 
